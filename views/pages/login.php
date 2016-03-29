fc524255d130b196fbf9eee12c2bf6e2d5380725
Deothan <mads@deothan.dk>
2016-03-29 14:19:43 +0200

    fixed

--- views/pages/login.php
+++ views/pages/login.php
@@ -12,7 +12,7 @@
         <li><button type="button" onClick="javascript:Validate()">Login</button></li>
     </ul>	
     </form>
-    
+
     <script>
 		function Validate(){
 			var name = document.getElementById("loginName").value;
@@ -20,13 +20,12 @@
 			
 			var request = new XMLHttpRequest();
 			request.open("POST", "/validate", true);
-			request.setRequestHeader("Content-Type", "application:json;charset=UTF-8");
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			request.send(JSON.stringify({"username":name, "password":pw}));	
 			
 			request.onreadystatechange = function(){
 				if(request.readyState == 4 && request.status == 200){
-					//var response = JSON.parse(request.responseText);
-					var response = JSON.parse('{"value":true}');
+					var response = JSON.parse(request.responseText);
 					if(response.value){
 						location.replace("/login");
 					}
